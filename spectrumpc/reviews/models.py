91cc70d95bc83d4c5e2bbad78b82ec50b5f7635a
Daniil Lutaev <daniillutaev1@gmail.com>
2022-03-10 16:26:06 +0300

    Приложение отзывы

---
+++ spectrumpc/reviews/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class reviews (models.Model):
+    reviews = models.TextField('Отзыв пользователя')
--- spectrumpc/spectrumpc/settings.py
+++ spectrumpc/spectrumpc/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'main',
     'computer',
+    'reviews',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
