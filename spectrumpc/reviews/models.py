32c7e703df0c91b8dd2abfda876be3325837b4de
Daniil Lutaev <daniillutaev1@gmail.com>
2022-03-11 21:08:29 +0300

    пп

--- spectrumpc/reviews/models.py
+++ spectrumpc/reviews/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
-class reviews (models.Model):
+
+class reviews(models.Model):
+    name = models.CharField('Имя пользователя', max_length=50)
     reviews = models.TextField('Отзыв пользователя')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
